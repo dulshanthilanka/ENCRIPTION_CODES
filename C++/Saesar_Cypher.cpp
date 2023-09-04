416d97c6838885e49556ac60175583c30ff30a6d
dulshanthilanka <88085689+dulshanthilanka@users.noreply.github.com>
2023-09-04 23:30:18 +0530

    update for most of symbels

--- C++/Saesar_Cypher.cpp
+++ C++/Saesar_Cypher.cpp
@@ -8,7 +8,40 @@ string encription(string recived_message, int value) {
     for (int i = 0; i < recived_message.length(); i++) {
         if (recived_message.at(i) == ' ') {
             encrypted_message += ' ';
-        } else {
+        }else if (recived_message.at(i) == '.') {
+            encrypted_message += '.';
+        }else if (recived_message.at(i) == ',') {
+            encrypted_message += ',';
+		 }else if (recived_message.at(i) == '"') {
+            encrypted_message += '"';
+		 }else if (recived_message.at(i) == '1') {
+            encrypted_message += '1';
+		 }else if (recived_message.at(i) == '2') {
+            encrypted_message += '2';
+		 }else if (recived_message.at(i) == '3') {
+            encrypted_message += '3';
+		 }else if (recived_message.at(i) == '4') {
+            encrypted_message += '4';
+		 }else if (recived_message.at(i) == '5') {
+            encrypted_message += '5';
+		 }else if (recived_message.at(i) == '6') {
+            encrypted_message += '6';
+		 }else if (recived_message.at(i) == '7') {
+            encrypted_message += '7';
+		 }else if (recived_message.at(i) == '8') {
+            encrypted_message += '8';
+		 }else if (recived_message.at(i) == '9') {
+            encrypted_message += '9';
+		 }else if (recived_message.at(i) == '/') {
+            encrypted_message += '/';
+		 }else if (recived_message.at(i) == '?') {
+            encrypted_message += '?';
+		 }else if (recived_message.at(i) == '(') {
+            encrypted_message += '(';
+		 }else if (recived_message.at(i) == ')') {
+            encrypted_message += ')';
+		 }
+		 else {
             encrypted_message += alphabet.at((alphabet.find(recived_message.at(i)) + value) % 26);
         }
     }
@@ -21,7 +54,39 @@ string decription(string recevid_encripted_message, int value) {
     for (int i = 0; i < recevid_encripted_message.length(); i++) {
         if (recevid_encripted_message.at(i) == ' ') {
             decripted_message += ' ';
-        } else {
+        }else if (recevid_encripted_message.at(i) == '.') {
+            decripted_message += '.';
+        }else if (recevid_encripted_message.at(i) == ',') {
+            decripted_message += ',';
+		 }else if (recevid_encripted_message.at(i) == '"') {
+            decripted_message += '"';
+		 }else if (recevid_encripted_message.at(i) == '1') {
+            decripted_message += '1';
+		 }else if (recevid_encripted_message.at(i) == '2') {
+            decripted_message += '2';
+		 }else if (recevid_encripted_message.at(i) == '3') {
+            decripted_message += '3';
+		 }else if (recevid_encripted_message.at(i) == '4') {
+            decripted_message += '4';
+		 }else if (recevid_encripted_message.at(i) == '5') {
+            decripted_message += '5';
+		 }else if (recevid_encripted_message.at(i) == '6') {
+            decripted_message += '6';
+		 }else if (recevid_encripted_message.at(i) == '7') {
+            decripted_message += '7';
+		 }else if (recevid_encripted_message.at(i) == '8') {
+            decripted_message += '8';
+		 }else if (recevid_encripted_message.at(i) == '9') {
+            decripted_message += '9';
+		 }else if (recevid_encripted_message.at(i) == '/') {
+            decripted_message += '/';
+		 }else if (recevid_encripted_message.at(i) == '?') {
+            decripted_message += '?';
+		 }else if (recevid_encripted_message.at(i) == '(') {
+            decripted_message += '(';
+		 }else if (recevid_encripted_message.at(i) == ')') {
+            decripted_message += ')';
+		 }else {
             decripted_message += alphabet.at(((alphabet.find(recevid_encripted_message.at(i)) - value + 26) % 26));
         }
     }
